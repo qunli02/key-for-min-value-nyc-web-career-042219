23bc1948f7b1f9249b264e9d7395ac8fb09e3b27
Qun Li Huang <qunli02@gmail.com>
2019-04-08 19:42:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)+
   low = 9999
+  key = nil
   name_hash.each do |name, value|  
     if value < low
       low = value
+      key = name
+    end
   end
-  
+  return key
 end

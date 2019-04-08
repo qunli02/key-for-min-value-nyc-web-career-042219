c1328d36146e2d87957b8d761d3988042392247f
Qun Li Huang <qunli02@gmail.com>
2019-04-08 19:41:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(name_hash)+
+  low = 9999
+  name_hash.each do |name, value|  
+    if value < low
+      low = value
+  end
+  
 end
